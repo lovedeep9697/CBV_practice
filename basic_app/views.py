4de6cc71bb6accc118e74451cac8800dd208fe27
lovedeep <lovedeepsingh9697@gmail.com>
2018-04-14 20:25:14 +0530

    models

--- basic_app/views.py
+++ basic_app/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-from django.views.generic import View,TemplateView
+from django.views.generic import View,TemplateView,ListView, DetailView
+from . import models
 # from django.http import HttpResponse
 # Create your views here.
 
@@ -11,11 +12,18 @@ from django.views.generic import View,TemplateView
 # 	def get(self,request):
 # 		return HttpResponse('Class Based Views')
 
-class IndexView(TemplateView):
-	template_name = 'index.html'
+# class IndexView(TemplateView):
+# 	template_name = 'index.html'
 
-	def get_context_data(self,**kwargs):
-		context = super().get_context_data(**kwargs)
+# 	def get_context_data(self,**kwargs):
+# 		context = super().get_context_data(**kwargs)
 
-		context['inject_me'] = 'Basic Injection'
-		return context 
+# 		context['inject_me'] = 'Basic Injection'
+# 		return context 
+
+class SchoolListView(ListView):
+	model = models.School
+
+class SchoolDetailView(DetailView):
+	model = models.School
+	template_name = 'basic_app/school_detail.html'
